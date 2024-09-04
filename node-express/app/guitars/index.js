82137c86df0b34306aa9ffccc45bf65f1930efd9
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-09-04 11:28:06 +0500

    Get request and response of guitar by id and make

--- node-express/app/guitars/index.js
+++ node-express/app/guitars/index.js
@@ -8,7 +8,10 @@ export const guitarRoutes = new Router();
 const guitars = [
     {id: 1, make: 'Fender', model: 'Stratocaster'},
     {id: 2, make: 'Gibson', model: 'Les Paul'},
-    {id: 3, make: 'Ibanez', model: 'RG'}
+    {id: 3, make: 'Ibanez', model: 'RG'},
+    {id: 4, make: 'Jackson', model: 'Soloist'},
+    {id: 5, make: 'ESP', model: 'Eclipse'},
+    {id: 6, make: 'Gibson', model: 'Custom 24'}
 ]
 
 // /guitars will be the route for all guitars
@@ -19,14 +22,25 @@ guitarRoutes.get('/', (req,res)=>{
 //routes paramerters
 guitarRoutes.get('/:id', (req,res)=>{
     const id =  parseInt(req.params.id, 10);
-    const guitar = guitars.find(g => g.id === id);
-    
-    //handle if id or guitar is not found
-    if(!guitar){
-        res.send(404);
+    // handling if id or by make, in URL  it should be /guitars/1 or /guitars/Fender
+    if(id){
+        //handle by id
+        const guitar = guitars.find(g => g.id === id);
+        //handle if id or guitar is not found
+        if(!guitar){
+            res.sendStatus(404);
+        }
+        else{
+            res.send(guitar);
+        }
+    }else{
+        //handle by make
+        const makeFound = guitars.filter(g => g.make.toLowerCase() === req.params.id.toLowerCase());
+        if(makeFound.length === 0){
+            res.sendStatus(404);
+        }
+        else{
+            res.send(makeFound);
+        }
     }
-    else{
-        res.send(guitar);
-    }
-    
 });
