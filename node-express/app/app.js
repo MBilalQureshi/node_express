747294223c75362e206dcda6764b2c8faf9b0552
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-09-04 11:41:10 +0500

    Perofrm simple addition

--- node-express/app/app.js
+++ node-express/app/app.js
@@ -3,9 +3,15 @@ import {guitarRoutes} from './guitars/index.js';
 
 const app = express();
 
+//guitar routes
 app.use('/guitars', guitarRoutes);
 app.get('/', (req, res)=>{
     res.send('Home Page')
+})
+// simple addition using app.get
+// app.get('/add/:num1/:num2',(req,res)=>{
+app.get('/add/:num1-:num2',(req,res)=>{
+    res.send(`${parseInt(req.params.num1)+ parseInt(req.params.num2)}`)
 })
 
 // app.get('/about', (req, res)=>{
